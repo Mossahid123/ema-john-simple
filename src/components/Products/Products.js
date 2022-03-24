a6a408e983a423497c6522b51b088ff7fb13a04f
Mossahid123 <mossahidhossain309@gmail.comm>
2022-03-24 12:31:15 +0600

    updating products menu

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -2,10 +2,15 @@ import React from 'react';
 import'./Products.css'
 
 const Products = (props) => {
-   const {id,img,price,seller,ratings} =props.product
+   const {name,img,price,seller,ratings} =props.product
     return (
         <div className='product'>
             <img src={img} alt=''></img>
+            <p>{name}</p>
+            <p><small>Price : {price}</small></p>
+            <p><small>Manufacturer : {seller}</small></p>
+            <p><small>Rating : {ratings} star</small></p>
+            <button>Add cart</button>
         </div>
     );
 };
